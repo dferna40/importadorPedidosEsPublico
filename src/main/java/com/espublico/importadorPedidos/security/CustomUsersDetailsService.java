c7174cf86e780532d368ddf14c99e69188548310
dferna40 <dferna40@gmail.com>
2024-01-16 14:51:58 +0100

    Añado cambios

--- src/main/java/com/espublico/importadorPedidos/security/CustomUsersDetailsService.java
+++ src/main/java/com/espublico/importadorPedidos/security/CustomUsersDetailsService.java
@@ -3,7 +3,6 @@ package com.espublico.importadorPedidos.security;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -14,7 +13,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.espublico.importadorPedidos.model.Roles;
-import com.espublico.importadorPedidos.model.User;
 import com.espublico.importadorPedidos.repository.UserRepository;
 
 @Service
