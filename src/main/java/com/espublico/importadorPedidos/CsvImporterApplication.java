3ce43dbbfa139c18a985d44cbfdb0d2bb4a272fa
David Fernandez Ramirez <dferna40@gmail.com>
2024-01-24 16:19:21 +0100

    Reestructuro la aplicacion desde cero

--- src/main/java/com/espublico/importadorPedidos/CsvImporterApplication.java
+++ src/main/java/com/espublico/importadorPedidos/CsvImporterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ImportadorPedidosApplication {
+public class CsvImporterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ImportadorPedidosApplication.class, args);
+		SpringApplication.run(CsvImporterApplication.class, args);
 	}
 
 }
--- src/main/java/com/espublico/importadorPedidos/security/SecurityConstants.java
+++
@@ -1,7 +0,0 @@
-package com.espublico.importadorPedidos.security;
-
-public class SecurityConstants {
-
-	public static final long JWT_EXPIRATION_TOKEN = 300000;
-	public static final String JWT_SIGNATURE = "SIGNATURE";
-}
